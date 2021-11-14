48ce3a106dd17ed987679e4bff71caa3aa4b6b13
Lendory <83349565+Lendory-really@users.noreply.github.com>
2021-11-14 11:50:15 +0300

    Delete data directory

--- data/guild.js
+++
@@ -1,4 +0,0 @@
-const schema = mongoose.Schema({
-    guildID: String
-});
-module.exports = mongoose.model("Guild", schema)
--- data/user.js
+++
@@ -1,8 +0,0 @@
-const schema = mongoose.Schema({
-    
-    userID: String,
-    guildID: String,
-    money: { type: Number, default: 0 },
-    workcd: {type: Number, default: 0}
-});
-module.exports = mongoose.model("User", schema)
